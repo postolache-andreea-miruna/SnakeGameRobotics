974aa0c7162d81e1f9ab7d057b9a2b1e7730be59
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2022-12-22 01:15:57 +0200

    Git do not copy my indentetion correct

--- GoodSnakeGame/lcdComands.h
+++ GoodSnakeGame/lcdComands.h
@@ -26,34 +26,34 @@ bool transitionToMenu = false;
 bool toMainMenu = false;
 
 byte doubleArrows[matrixSize] = {
-	0b00100,
-	0b01010,
-	0b10001,
-	0b00000,
-	0b00000,
-	0b10001,
-	0b01010,
-	0b00100
+  0b00100,
+  0b01010,
+  0b10001,
+  0b00000,
+  0b00000,
+  0b10001,
+  0b01010,
+  0b00100
 };
 byte downArrow[matrixSize] = {
-	0b00000,
-	0b00000,
-	0b00000,
-	0b10001,
-	0b01010,
-	0b00100,
-	0b00000,
-	0b00000
+  0b00000,
+  0b00000,
+  0b00000,
+  0b10001,
+  0b01010,
+  0b00100,
+  0b00000,
+  0b00000
 };
 byte upArrow[matrixSize] = {
-	0b00000,
-	0b00000,
-	0b00100,
-	0b01010,
-	0b10001,
-	0b00000,
-	0b00000,
-	0b00000
+  0b00000,
+  0b00000,
+  0b00100,
+  0b01010,
+  0b10001,
+  0b00000,
+  0b00000,
+  0b00000
 };
 byte heart[matrixSize] = {
   0b00000,
@@ -77,13 +77,13 @@ byte smiley[matrixSize] = {
 };
 byte buttonPress[matrixSize] = {
   0b00000,
-	0b00000,
-	0b00000,
-	0b01110,
-	0b01110,
-	0b11111,
-	0b11111,
-	0b11111
+  0b00000,
+  0b00000,
+  0b01110,
+  0b01110,
+  0b11111,
+  0b11111,
+  0b11111
 };
 
 void greeting()  //use when the app is open
